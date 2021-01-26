016f6786300cda710e925c9e11ed6959346d6b3c
TEO WEN ZHI <wenzhi5168@gmail.com>
2021-01-26 22:36:29 +0800

    Initial commit

---
+++ Layout.js
@@ -0,0 +1,25 @@
+﻿function navM() {
+    let x = document.getElementById("topNav");
+    let y = document.getElementById("back");
+    let w = document.getElementsByTagName("body")[0];
+    let z = document.getElementById("icon");
+
+    if (w.style.overflow != "hidden") {
+        if (window.innerWidth <= 500)
+            w.style.overflow = "hidden"
+    }
+    else {
+        w.style.overflow = "initial"
+    }
+
+
+    x.classList.toggle("active");
+    y.classList.toggle("active");
+    z.classList.toggle("active");
+
+}
+
+function clear() {
+    let w = document.getElementsByTagName("body")[0];
+    w.style.overflow = "initial";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment-Template
+ Assignment template for WEB APPLICATION
