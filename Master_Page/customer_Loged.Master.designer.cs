f2c54d88777edff90020e7009690ff0fda5bc0cf
kee208 <ruilin0208@gmail.com>
2021-02-28 02:22:46 +0800

    master的header不懂做莫有问题

--- Master_Page/customer_Loged.Master.designer.cs
+++ Master_Page/customer_Loged.Master.designer.cs
@@ -7,11 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Assignment_Template.Master_Page
+namespace Assignment_Template
 {
 
 
-    public partial class Customer_Loged
+    public partial class Site1
     {
 
         /// <summary>
@@ -31,15 +31,6 @@ namespace Assignment_Template.Master_Page
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// LoginStatus1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginStatus LoginStatus1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
