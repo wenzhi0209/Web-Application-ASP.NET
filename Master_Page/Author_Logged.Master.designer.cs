cf922d9c04fd81918aeb5aa18991111cac8e78f0
TEO WEN ZHI <45276690+wenzhi0209@users.noreply.github.com>
2021-04-10 07:47:22 +0800

    Update master page link

--- Master_Page/Author_Logged.Master.designer.cs
+++ Master_Page/Author_Logged.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Assignment_Template.Master_Page
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// HyperLink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
-
-        /// <summary>
         /// LoginStatus1 control.
         /// </summary>
         /// <remarks>
